c94598f5829ef8d74415a561cff0fd1b9d6557be
Ryan <rdmaloney3@gmail.com>
2021-02-22 18:49:37 -0500

    basic layout for feed

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import Posts from '../Posts'
-import "./styles.css"
+import React from "react";
+import Posts from "../Posts";
+import NavBar from "../NavBar";
+import StationarySide from "../other/.Home/StationarySide";
+import "./styles.css";
 
 const LandingPage = () => {
+  // posts = fetch("/");
 
-    return (
-        <div className="post">
-            <Posts />
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+      <div className='landing__container'>
+        <div className='posts__feed'>
+          <Posts />
         </div>
-    )
-
-
-
-
-
-}
+        <div>
+          <StationarySide />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
