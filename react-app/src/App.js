7ae32e3d6162c3a79dcb1ad29d7300541c5c6e1e
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-02-22 16:35:16 -0500

    s
    s

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar/index";
 import NavBar from "./components/NavBar";
 import Home from './components/Home'
 import Splash from './components/SplashPage'
--- react-app/src/components/ProfilePage/Header.js
+++ react-app/src/components/ProfilePage/Header.js
@@ -10,6 +10,7 @@ function Header() {
             "https://starecat.com/content/wp-content/uploads/pope-francis-punch-man-with-damaged-face-photoshopped.jpg",
             "https://i.kym-cdn.com/entries/icons/original/000/027/269/Screen_Shot_2018-09-28_at_3.14.37_PM.png"
         ]
+
     return (
 
         <>
