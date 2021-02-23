7a655ed434649f45959e4cbdb2472cabd760cc6c
Ryan <rdmaloney3@gmail.com>
2021-02-23 17:29:00 -0500

    finished styling post and comment form

--- react-app/src/components/Comments/CommentForm.js
+++ react-app/src/components/Comments/CommentForm.js
@@ -3,7 +3,7 @@ import "./comments.css";
 
 const CommentForm = () => {
   return (
-    <>
+    <div className='comment-form__container'>
       <img
         className='comment-form__icon'
         src='/icons/insta_smily_face_icon.png'
@@ -12,7 +12,12 @@ const CommentForm = () => {
         className='comment-form__input'
         placeholder='Add a comment...'
       ></input>
-    </>
+      <form action='/' method='post'>
+        <button type='submit' className='comment-form__button'>
+          Post
+        </button>
+      </form>
+    </div>
   );
 };
 
