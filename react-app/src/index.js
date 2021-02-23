6a587f0d42d7e4ce0151dac8dfa9036a055e5bc1
Ryan Conk <ryangconk@gmail.com>
2021-02-23 09:50:01 -0700

    add the thunk store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 // import { ModalProvider } from './context/Modal.js';
-
+import configureStore from './Store'
 import './index.css';
 import App from './App';
+
+const store = configureStore()
+
 
 
 function Root() {
   return (
+    <Provider store={store}>
 
-    <BrowserRouter>
-      {/* <ModalProvider> */}
+      <BrowserRouter>
+        {/* <ModalProvider> */}
 
-      <App />
-      {/* </ModalProvider> */}
-    </BrowserRouter>
+        <App />
+        {/* </ModalProvider> */}
+      </BrowserRouter>
+    </Provider>
 
   );
 }
