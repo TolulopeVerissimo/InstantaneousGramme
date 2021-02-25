bbadc68053e485183f334983dd280288c0eeedd3
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-02-25 15:46:48 -0500

    Update index.js

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -4,29 +4,31 @@ import { useDispatch, useSelector } from 'react-redux'
 // import FeaturedStories from './FeaturedStories.js'
 // import Posts from './Posts.js'
 import { getProfile } from '../../Store/profile'
-// import { getFollows } from '../../Store/follow'
-// import { getUsers } from '../../Store/user'
+import { getFollows } from '../../Store/follow'
+import { getPosts } from '../../Store/posts'
+import { getUsers } from '../../Store/user'
 import { useParams } from 'react-router-dom'
 function Profile() {
-  const { id } = useParams()
-  const dispatch = useDispatch()
-	const profile = useSelector(state=>state.profile)
-
-
-	useEffect(() => {
-			debugger;
-			dispatch(getProfile(id))
-			// dispatch(getFollows(id))
-			// dispatch(getUsers())
+    const profile = useSelector(state => state.profiles)
+    const user = useSelector(state => state.users)
+    const follow = useSelector(state => state.follows)
+    const post = useSelector(state => state.posts)
+    const { id } = useParams()
+    const dispatch = useDispatch()
+    useEffect(() => {
+	debugger;
+	dispatch(getProfile(id))
+	// dispatch(getFollows(id))
+	// dispatch(getUsers())
 	}, [dispatch])
 
 	return (
 			<>
 					{profile &&  <h2>hi {profile.username}</h2>}
 
-					{/* <Header /> */}
-					{/* <FeaturedStories /> */}
-					{/* <Posts /> */}
+				  <Header profile={profile} user={user} />
+            {/* <FeaturedStories /> */}
+            {/* <Posts /> */}
 			</>
 	)
 }
