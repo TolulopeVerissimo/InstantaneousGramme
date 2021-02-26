1a653a62ae1775a171fa7c725e42d5f015e51960
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-02-26 14:04:44 -0500

    autosave

--- react-app/src/components/NewPostModal/NewPostForm.js
+++ react-app/src/components/NewPostModal/NewPostForm.js
@@ -12,20 +12,20 @@ function NewPostForm() {
   const user = useSelector(state => state.session.user)
   let userId
   if (user) {
-    userId=user.id
+    userId = user.id
   }
-  const handleSubmit= async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const url = await getSignedRequest(photo)
     setImagePath(url)
     const options =
-                    {
-                      method: 'POST',
-                      headers: {
-                        'Content-Type':'Application/json'
-                      },
-                      body: JSON.stringify({isPrivate, description, imagePath, userId})
-                    }
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json'
+      },
+      body: JSON.stringify({ isPrivate, description, imagePath, userId })
+    }
     const res = await fetch('/api/posts/', options)
     return res
   }
@@ -36,15 +36,15 @@ function NewPostForm() {
         <input
           type="file"
           className="newpostform__input fileInput"
-          onChange={(e) => setPhoto(e.target.files[0])}/>
+          onChange={(e) => setPhoto(e.target.files[0])} />
       </label>
-        <textarea
-          rows="5"
-          cols="33"
-          type="textarea"
-          value={description}
-          placeholder="Enter a description"
-          onChange={(e) => setDescription(e.target.value)} />
+      <textarea
+        rows="5"
+        cols="33"
+        type="textarea"
+        value={description}
+        placeholder="Enter a description"
+        onChange={(e) => setDescription(e.target.value)} />
 
       <label className="newpostform__label">
         Private
