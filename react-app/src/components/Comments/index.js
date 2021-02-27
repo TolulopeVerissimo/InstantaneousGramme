1cafa7d3edd3f1d93f0939fe2c4b6d90b0d5cd67
Ryan Conk <ryangconk@gmail.com>
2021-02-26 17:11:28 -0700

    all is working

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -1,32 +1,32 @@
 import React, {useState,useEffect} from "react";
-import "./comments.css";
+import "./.css";
 
 
 
 const Comments = (props) => {
-  const [comments, setComments] = useState([]);
+  const [, setComments] = useState([]);
   console.log(props.postId);
   const [isLoaded,setIsLoaded] = useState(false)
   useEffect(() => {
     (async () => {
-      const response = await fetch(`/api/comments/${props.postId}`);
-      const comments = await response.json();
-      console.log("sdsdsd", comments.comments);
-      setComments(comments);
-      if(comments) setIsLoaded(true)
+      const response = await fetch(`/api//${props.postId}`);
+      const  = await response.json();
+      console.log("sdsdsd", .);
+      setComments();
+      if() setIsLoaded(true)
     })();
-  }, [comments]);
+  }, []);
 
   return (
     <>
       {isLoaded &&
-        comments.comments.map((comments) => (
+        ..map(() => (
           <div className='comments__container'>
-            {/* if( comments.length > 2) collapse content  
-                  "View all 152 comments" */}
+            {/* if( .length > 2) collapse content  
+                  "View all 152 " */}
             <div className='comments__user-comment'>
-              <div className='comment__username'>{comments.username}</div>
-              <div className='comment__content'>{comments.content}</div>
+              <div className='comment__username'>{.username}</div>
+              <div className='comment__content'>{.content}</div>
             </div>
           </div>
 
