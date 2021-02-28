9e8d60f205ed939abb0d28a11f032be21033dae2
TolulopeVerissimo <t.e.veris15@gmail.com>
2021-02-28 18:06:50 -0500

    css
    css

--- react-app/src/components/Posts/Post.js
+++ react-app/src/components/Posts/Post.js
@@ -51,8 +51,9 @@ const Post = ({ post, user }) => {
         <div className='post__user-info'>
           <div onMouseEnter={enterMouse} onMouseLeave={leaveMouse} className='post__username'>
             {post.username}
+            {hover && hover ? <Hover open={hover} post={post} profile={profiles}></Hover> : ""}
             <div></div>
-            {/* <Hover open={hover} post={post} profile={profiles}></Hover> */}
+
           </div>
 
         </div>
