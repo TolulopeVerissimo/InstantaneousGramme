f968a2fd3321fd5e68620827c40c6b27db0a628d
Ryan <rdmaloney3@gmail.com>
2021-03-31 15:09:09 -0400

    added infinite scroll library and added to posts feed

--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import InfiniteScroll from "react-infinite-scroller";
 import Comments from "../Comments";
 import CommentForm from "../Comments/CommentForm";
 
@@ -12,20 +13,50 @@ const Posts = () => {
     Object.values(state.posts).filter((post) => post.userId !== user.id)
   );
 
+  let count = 0;
   const [isLoaded, setIsLoaded] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [postsToDisplay, setPostsToDisplay] = useState([]);
   const dispatch = useDispatch();
 
+  const loadFunc = () => {
+    //need to check for end of posts
+    for (let i = 0; i < 10; i++) {
+      postsToDisplay.push(posts[count]);
+      count++;
+    }
+    console.log(count, postsToDisplay);
+  };
 
   useEffect(() => {
-    if (posts && user) setIsLoaded(true);
-  }, [posts, user]);
+    if (posts) {
+      for (let i = 0; i < 10; i++) {
+        postsToDisplay.push(posts[count]);
+        count++;
+      }
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    if (postsToDisplay && user) setIsLoaded(true);
+  }, [postsToDisplay, user]);
 
   return (
     <>
-      {isLoaded &&
-        posts.map((post) => (
+      <InfiniteScroll
+        hasMore={hasMore}
+        loadMore={loadFunc}
+        loader={
+          <div className='loader' key={0}>
+            Loading ...
+          </div>
+        }
+      >
+        {isLoaded &&
+          postsToDisplay.map((post) => (
             <Post key={post.id} post={post} user={user} />
-        ))}
+          ))}
+      </InfiniteScroll>
     </>
   );
 };
