d7273f7c4474b2c62b4dee25a321ec07d4a658b0
Ryan <rdmaloney3@gmail.com>
2021-04-06 17:00:04 -0400

    regex test working

--- react-app/src/components/NavBar/SearchBar.js
+++ react-app/src/components/NavBar/SearchBar.js
@@ -6,7 +6,9 @@ const SearchBar = () => {
   const [query, setQuery] = useState("");
 
   let regex = new RegExp(`w*${query}w*`, "i");
-  const searchResults = useSelector((state) => state.users.username);
+  const searchResults = useSelector((state) =>
+    Object.values(state.users).filter((user) => regex.test(user.username))
+  );
   console.log(searchResults);
 
   //state.users.filter((user) => regex.test(user.username))
@@ -25,7 +27,11 @@ const SearchBar = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       ></input>
-      <ul>{searchResults}</ul>
+      <ul>
+        {searchResults.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
     </div>
   );
 };
